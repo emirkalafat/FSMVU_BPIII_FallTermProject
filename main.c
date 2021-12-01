80804b89c468d7f6ff1cfb58259d24e18a2a055a
Emir Kalafat <emirklft@outlook.com>
2021-12-01 15:11:21 +0300

    main init

---
+++ main.c
@@ -0,0 +1,7 @@
+/*
+* @file main.c
+* @description executes main program functions
+* @assignment First Misson
+* @date 1.12.2021
+* @author Ahmet Emir Kalafat ahmetemir.kalafat@stu.fsm.edu.tr
+*/
---
+++ matrisoperations.h
@@ -0,0 +1,7 @@
+/*
+* @file matrisoperations.h
+* @description ///"güncellenecek"executes main program functions///
+* @assignment First Misson
+* @date 1.12.2021
+* @author Ahmet Emir Kalafat ahmetemir.kalafat@stu.fsm.edu.tr
+*/
